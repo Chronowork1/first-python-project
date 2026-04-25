a32e62a58828f6d9db7cdf6cd2dd0fb5ce509165
Jian Xie <Jianxie6464@gmail.com>
2026-04-26 03:02:09 +1000

    Slight edit

--- python.py
+++ python.py
@@ -1,2 +1,5 @@
-for i in range(8):
-    print(i);
+name = "Alex"
+age = "20"
+favorite_food = "Pizza"
+
+print("name = "+ name + "\n", "age = " + age + "\n", "favorite food = " + favorite_food + "\n");
